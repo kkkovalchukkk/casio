331d4f4d247727d6da35e013115898ccbce674e4
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-11-15 16:45:53 +0300

    changed map-viewer

--- scripts/drag.js
+++ scripts/drag.js
@@ -25,49 +25,13 @@ window.addEventListener('DOMContentLoaded', () => {
     '.map-viewer__current-location'
   );
 
-  let currentScale = 100;
-  let isDragging = false;
-  let startX, startY, initialX, initialY;
+  const container = document.getElementById('myPanzoom');
+
+  new Panzoom(container, {
+    //
+  });
+
   let hintTouched = false;
-
-  const startDrag = (e) => {
-    isDragging = true;
-    startX = e.clientX;
-    startY = e.clientY;
-
-    if (e.changedTouches && e.changedTouches.length) {
-      startX = e.changedTouches[0].clientX;
-      startY = e.changedTouches[0].clientY;
-    }
-    initialX = mapViewerContentEl.offsetLeft;
-    initialY = mapViewerContentEl.offsetTop;
-
-    document.addEventListener('mousemove', drag);
-    document.addEventListener('touchmove', drag);
-    document.addEventListener('mouseup', stopDrag);
-    document.addEventListener('touchend', stopDrag);
-  };
-
-  const drag = (e) => {
-    if (isDragging) {
-      let deltaX = e.clientX - startX;
-      let deltaY = e.clientY - startY;
-
-      if (e.changedTouches && e.changedTouches.length) {
-        deltaX = e.changedTouches[0].clientX - startX;
-        deltaY = e.changedTouches[0].clientY - startY;
-      }
-
-      mapViewerContentEl.style.left = initialX + deltaX + 'px';
-      mapViewerContentEl.style.top = initialY + deltaY + 'px';
-    }
-  };
-
-  const stopDrag = () => {
-    isDragging = false;
-    document.removeEventListener('mousemove', drag);
-    document.removeEventListener('mouseup', stopDrag);
-  };
 
   mapViewerContentEl
     .querySelectorAll('img')
@@ -83,26 +47,22 @@ window.addEventListener('DOMContentLoaded', () => {
         hintTouched = true;
         return;
       }
-      currentScale = 100;
       mapViewerHomeSlideEl.classList.remove('map-viewer__content-item--active');
       mapViewerInsideLocationEls[idx].classList.add(
         'map-viewer__content-item--active'
       );
-      mapViewerContentEl.style.left = 0 + 'px';
-      mapViewerContentEl.style.top = 0 + 'px';
-      mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
       mapViewerMenuEl.classList.add('map-viewer__menu--active');
       mapViewerCurrentLocationEl.textContent = idx + 1 + ' палуба';
       hintTouched = false;
     });
 
-    h.addEventListener('touch', () => {
-      e.preventDefault();
-    });
+    // h.addEventListener('touch', () => {
+    //   e.preventDefault();
+    // });
 
-    h.addEventListener('mousedown', (e) => {
-      e.stopPropagation();
-    });
+    // h.addEventListener('mousedown', (e) => {
+    //   e.stopPropagation();
+    // });
   });
 
   mapViewerOpenBtnEls.forEach((btn) =>
@@ -111,12 +71,13 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   );
 
+  mapViewerFullScreenScaleBtnEl.addEventListener('click', () => {
+    mapViewerEl.classList.toggle('map-viewer--fullscreen');
+  });
+
   mapViewerCloseBtnEl.addEventListener('click', () => {
-    currentScale = 100;
     mapViewerEl.classList.remove('map-viewer--active');
-    mapViewerContentEl.style.left = 0 + 'px';
-    mapViewerContentEl.style.top = 0 + 'px';
-    mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
+    mapViewerMenuEl.classList.remove('map-viewer__menu--active');
     mapViewerInsideLocationEls.forEach((loc) =>
       loc.classList.remove('map-viewer__content-item--active')
     );
@@ -124,37 +85,11 @@ window.addEventListener('DOMContentLoaded', () => {
     mapViewerEl.classList.remove('map-viewer--fullscreen');
   });
 
-  mapViewerPlusScaleBtnEl.addEventListener('click', () => {
-    if (currentScale === 160) return;
-    currentScale += 20;
-    mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
-  });
-  mapViewerMinusScaleBtnEl.addEventListener('click', () => {
-    if (currentScale === 40) return;
-    currentScale -= 20;
-    mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
-  });
-
   mapViewerGoBackBtnEl.addEventListener('click', () => {
-    currentScale = 100;
     mapViewerMenuEl.classList.remove('map-viewer__menu--active');
     mapViewerInsideLocationEls.forEach((loc) =>
       loc.classList.remove('map-viewer__content-item--active')
     );
     mapViewerHomeSlideEl.classList.add('map-viewer__content-item--active');
-    mapViewerContentEl.style.left = 0 + 'px';
-    mapViewerContentEl.style.top = 0 + 'px';
-    mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
   });
-
-  mapViewerFullScreenScaleBtnEl.addEventListener('click', () => {
-    currentScale = 100;
-    mapViewerEl.classList.toggle('map-viewer--fullscreen');
-    mapViewerContentEl.style.left = 0 + 'px';
-    mapViewerContentEl.style.top = 0 + 'px';
-    mapViewerContentEl.style.transform = `scale(${currentScale}%)`;
-  });
-
-  mapViewerContentEl.addEventListener('mousedown', startDrag);
-  mapViewerContentEl.addEventListener('touchstart', startDrag);
 });
